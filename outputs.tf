8fa7dd434340368750318aa1810067ac7508c088
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:20 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "network_manager_routing_rules" {
-  description = "All network_manager_routing_rule resources"
-  value       = azurerm_network_manager_routing_rule.network_manager_routing_rules
-}
 output "network_manager_routing_rules_description" {
-  description = "List of description values across all network_manager_routing_rules"
-  value       = [for k, v in azurerm_network_manager_routing_rule.network_manager_routing_rules : v.description]
+  description = "Map of description values across all network_manager_routing_rules, keyed the same as var.network_manager_routing_rules"
+  value       = { for k, v in azurerm_network_manager_routing_rule.network_manager_routing_rules : k => v.description }
 }
 output "network_manager_routing_rules_destination" {
-  description = "List of destination values across all network_manager_routing_rules"
-  value       = [for k, v in azurerm_network_manager_routing_rule.network_manager_routing_rules : v.destination]
+  description = "Map of destination values across all network_manager_routing_rules, keyed the same as var.network_manager_routing_rules"
+  value       = { for k, v in azurerm_network_manager_routing_rule.network_manager_routing_rules : k => v.destination }
 }
 output "network_manager_routing_rules_name" {
-  description = "List of name values across all network_manager_routing_rules"
-  value       = [for k, v in azurerm_network_manager_routing_rule.network_manager_routing_rules : v.name]
+  description = "Map of name values across all network_manager_routing_rules, keyed the same as var.network_manager_routing_rules"
+  value       = { for k, v in azurerm_network_manager_routing_rule.network_manager_routing_rules : k => v.name }
 }
 output "network_manager_routing_rules_next_hop" {
-  description = "List of next_hop values across all network_manager_routing_rules"
-  value       = [for k, v in azurerm_network_manager_routing_rule.network_manager_routing_rules : v.next_hop]
+  description = "Map of next_hop values across all network_manager_routing_rules, keyed the same as var.network_manager_routing_rules"
+  value       = { for k, v in azurerm_network_manager_routing_rule.network_manager_routing_rules : k => v.next_hop }
 }
 output "network_manager_routing_rules_rule_collection_id" {
-  description = "List of rule_collection_id values across all network_manager_routing_rules"
-  value       = [for k, v in azurerm_network_manager_routing_rule.network_manager_routing_rules : v.rule_collection_id]
+  description = "Map of rule_collection_id values across all network_manager_routing_rules, keyed the same as var.network_manager_routing_rules"
+  value       = { for k, v in azurerm_network_manager_routing_rule.network_manager_routing_rules : k => v.rule_collection_id }
 }
 
